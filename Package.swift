91b61865c5298bf32e2b1a979d354f018d40fcbd
Niranjan Bhaskar <tradingwithninja95@gmail.com>
2025-09-17 13:32:58 -0400

    update SPM package

--- Package.swift
+++ Package.swift
@@ -18,11 +18,13 @@ let package = Package(
     products: [
         .library(
             name: "WS1IntelligenceSDK",
-            targets: ["WS1IntelligenceSDK", "FIPS"]),
+            targets: ["OmnissaCryptoSDK", "WS1IntelligenceSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/euc-releases/ws1-crypto-sdk.git", from: "25.06.0")
     ],
     targets: [
-        .binaryTarget(name: "WS1IntelligenceSDK", url: "https://github.com/euc-releases/ws1-intelligencesdk-sdk-ios/releases/download/25.4.1/WS1IntelligenceSDK.xcframework.zip", checksum:"85bc5fc3aac18f39fb51577e2b9905be3b5d09386c64965b22f157210547026d"),
-        .binaryTarget(name: "FIPS", url: "https://github.com/euc-releases/iOS-WorkspaceONE-SDK/releases/download/25.04.1/fips.xcframework.zip", checksum:"e289f84164a4086229823b56ecbf5e83330a7bc3cf025bdd4e7c758d7b953272")
+        .binaryTarget(name: "WS1IntelligenceSDK", url: "https://github.com/euc-releases/ws1-intelligencesdk-sdk-ios/releases/download/25.7.0/WS1IntelligenceSDK.xcframework.zip", checksum: "e0d4d85f014eec90fdc26de70c845ec5d9abe24ebc27814ed6621130668c892e"),
+        .target(name: "OmnissaCryptoSDK", dependencies: [.product(name: "WS1CryptoSDK", package: "ws1-crypto-sdk")])
     ]
 )
-
